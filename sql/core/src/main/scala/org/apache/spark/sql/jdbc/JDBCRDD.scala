ba3b7c83374859a1399c25a1211a8fa4d6ffeda7
chenfangfang <chenfangfang@ucfgroup.com>
2015-08-26 11:28:46 +0800

    SPARK-10220
    	org.apache.spark.sql.jdbc.JDBCRDD could not parse mysql table column named reserved word

--- sql/core/src/main/scala/org/apache/spark/sql/jdbc/JDBCRDD.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/jdbc/JDBCRDD.scala
@@ -234,6 +234,12 @@ private[sql] class JDBCRDD(
     columns.foreach(x => sb.append(",").append(x))
     if (sb.length == 0) "1" else sb.substring(1)
   }
+  
+  private val columnList_backquote: String = {
+    val sb = new StringBuilder()
+    columns.foreach(x => sb.append(",").append("`"+x+"`"))
+    if (sb.length == 0) "1" else sb.substring(1)
+  }
 
   /**
    * Converts value to SQL expression.
@@ -341,7 +347,7 @@ private[sql] class JDBCRDD(
 
     val myWhereClause = getWhereClause(part)
 
-    val sqlText = s"SELECT $columnList FROM $fqTable $myWhereClause"
+    val sqlText = s"SELECT $columnList_backquote FROM $fqTable $myWhereClause"
     val stmt = conn.prepareStatement(sqlText,
         ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
     val rs = stmt.executeQuery()
